b956ed002a15d0523359a02dde25facad2bee5c3
theeasylearn <theeasylearn@gmail.com>
2026-02-13 12:08:51 +0530

    relational operator

---
+++ 13_arthimatic_operator.py
@@ -0,0 +1,25 @@
+num1 = int(input("Enter number"))
+num2 = int(input("Enter another number"))
+
+result = num1 + num2 
+print(f"{result} = {num1} + {num2}")
+
+result = num1 - num2 
+print(f"{result} = {num1} - {num2}")
+
+result = num1 * num2 
+print(f"{result} = {num1} * {num2}")
+
+
+result = num1 / num2 
+print(f"{result} = {num1} / {num2}")
+
+
+result = num1 % num2 
+print(f"{result} = {num1} % {num2}")
+
+result = num1 // num2 
+print(f"{result} = {num1} // {num2}")
+
+result = num1 ** num2 
+print(f"{result} = {num1} ** {num2}")
---
+++ 14_relational_operator.py
@@ -0,0 +1,25 @@
+num1 = 10 
+num2 = 11 
+
+num1 = int(input("Enter num 1"))
+num2 = int(input("Enter num 2"))
+
+#False =  10  == 11
+result = num1 == num2 
+print(f"{result} = {num1} == {num2}")
+
+result = num1 != num2 
+print(f"{result} = {num1} != {num2}")
+
+result = num1 < num2 
+print(f"{result} = {num1} < {num2}")
+
+result = num1 > num2 
+print(f"{result} = {num1} > {num2}")
+
+result = num1 <= num2 
+print(f"{result} = {num1} <= {num2}")
+
+result = num1 >= num2 
+print(f"{result} = {num1} >= {num2}")
+
